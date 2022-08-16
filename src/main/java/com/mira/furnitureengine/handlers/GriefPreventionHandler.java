afddb902f23d201a11ebf97a2da1f24466d6ab4f
James Lyne <jim+github@not-null.co.uk>
2022-08-16 18:12:25 +0100

    Fix griefprevention deprecated usage

--- src/main/java/com/mira/furnitureengine/handlers/GriefPreventionHandler.java
+++ src/main/java/com/mira/furnitureengine/handlers/GriefPreventionHandler.java
@@ -1,12 +1,15 @@
 package com.mira.furnitureengine.handlers;
 
 import me.ryanhamshire.GriefPrevention.Claim;
+import me.ryanhamshire.GriefPrevention.ClaimPermission;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+
+import java.util.function.Supplier;
 
 public class GriefPreventionHandler {
 	private final boolean gpEnabled;
@@ -29,9 +32,14 @@ public class GriefPreventionHandler {
 
 		Claim claim = griefPrevention.dataStore.getClaimAt(block.getLocation(), false, null);
 
-		if(claim != null && claim.allowBuild(player, block.getType()) != null) {
-			player.sendMessage(Component.text(claim.allowBuild(player, block.getType())).color(NamedTextColor.RED));
+		if(claim == null) {
+			return true;
+		}
 
+		Supplier<String> denialMessage = claim.checkPermission(player, ClaimPermission.Build, null);
+
+		if(denialMessage != null) {
+			player.sendMessage(Component.text(denialMessage.get()).color(NamedTextColor.RED));
 			return false;
 		}
 
