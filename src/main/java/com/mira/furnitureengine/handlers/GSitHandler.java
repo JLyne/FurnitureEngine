364ff0b8f434557fe396ab2d7a4b03bb94b9c9e7
James Lyne <jim+github@not-null.co.uk>
2025-02-23 20:53:10 +0000

    Update gsit

--- src/main/java/com/mira/furnitureengine/handlers/GSitHandler.java
+++ src/main/java/com/mira/furnitureengine/handlers/GSitHandler.java
@@ -1,19 +1,22 @@
 package com.mira.furnitureengine.handlers;
 
-import dev.geco.gsit.objects.GetUpReason;
-import org.bukkit.Bukkit;
+import dev.geco.gsit.object.GStopReason;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import dev.geco.gsit.api.GSitAPI;
 
 public class GSitHandler {
 	public void sit(Block seat, Player player, double offset) {
-		if(GSitAPI.getSeats(seat).size() == 0 && GSitAPI.canSit(player) && !GSitAPI.isSitting(player) && !GSitAPI.isPosing(player)) {
+		if(GSitAPI.getSeatsByBlock(seat).isEmpty()
+				&& GSitAPI.canEntityUseSit(player)
+				&& !GSitAPI.isEntitySitting(player)
+				&& !GSitAPI.isPlayerPosing(player)
+				&& !GSitAPI.isPlayerCrawling(player)) {
 			GSitAPI.createSeat(seat, player, true, 0, -0.4 + offset, 0, 0f, true);
 		}
 	}
 
 	public void removeSeats(Block block) {
-		GSitAPI.getSeats(block).forEach(seat -> GSitAPI.removeSeat(seat.getEntity(), GetUpReason.BREAK, true));
+		GSitAPI.getSeatsByBlock(block).forEach(seat -> GSitAPI.removeSeat(seat, GStopReason.BLOCK_BREAK, true));
 	}
 }
