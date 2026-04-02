1590663cff488b2f0c8852d3862e3ef994546eaa
James Lyne <jim+github@not-null.co.uk>
2026-04-02 15:32:22 +0100

    Use Purpur's setPredicate when possible to exclude custom items from this and other plugins in crafting recipes

--- src/main/java/com/mira/furnitureengine/RecipeManager.java
+++ src/main/java/com/mira/furnitureengine/RecipeManager.java
@@ -2,13 +2,19 @@ package com.mira.furnitureengine;
 
 import com.mira.furnitureengine.utils.ItemUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ItemType;
+import org.bukkit.inventory.RecipeChoice;
 import org.bukkit.inventory.ShapedRecipe;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Predicate;
 
 public class RecipeManager {
 	private final FurnitureEngine plugin;
@@ -44,13 +50,40 @@ public class RecipeManager {
 			String[] shape = furnitureConfig.getStringList(key + ".crafting.recipe").toArray(new String[0]);
 			String group = furnitureConfig.getString(key + ".crafting.group", "");
 			ConfigurationSection ingredientSection = furnitureConfig.getConfigurationSection(key + ".crafting.ingredients");
-			Map<Character, Material> ingredients = new HashMap<>();
+			Map<Character, RecipeChoice> ingredients = new HashMap<>();
 
 			try {
 				if(ingredientSection != null) {
 					ingredientSection.getKeys(false)
-							.forEach(character -> ingredients.put(character.charAt(0),
-															 Material.matchMaterial(ingredientSection.getString(character, ""))));
+							.forEach(character -> {
+								NamespacedKey itemTypeName = NamespacedKey.fromString(ingredientSection.getString(character, ""));
+
+								if(itemTypeName == null) {
+									throw new IllegalArgumentException("Invalid recipe ingredient for " + key + ": " + itemTypeName);
+								}
+
+								ItemType itemType = Registry.ITEM.get(itemTypeName);
+
+								if(itemType == null) {
+									throw new IllegalArgumentException("Invalid recipe ingredient for " + key + ": " + itemTypeName);
+								}
+
+								RecipeChoice choice;
+
+								// Use Purpur's setPredicate when possible to exclude custom items from this and other plugins
+								// in crafting recipes
+								try {
+									choice = new RecipeChoice.ExactChoice(itemType.createItemStack());
+									Method setPredicate = choice.getClass().getMethod("setPredicate", Predicate.class);
+									Predicate<ItemStack> predicate = (ItemStack i) ->
+											itemType.equals(i.getType().asItemType()) && i.getPersistentDataContainer().isEmpty();
+									setPredicate.invoke(choice, predicate);
+								} catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+									choice = RecipeChoice.itemType(itemType);
+								}
+
+								ingredients.put(character.charAt(0), choice);
+							});
 				}
 
 				ShapedRecipe recipe = new ShapedRecipe(recipeKey, ItemUtils.createFurnitureItem(furniture, 1));
@@ -67,7 +100,7 @@ public class RecipeManager {
 	}
 
 	public void unregisterRecipes() {
-		recipes.forEach((key, recipe) -> Bukkit.removeRecipe(key));
+		recipes.forEach((key, _) -> Bukkit.removeRecipe(key));
 		recipes.clear();
 	}
 }
